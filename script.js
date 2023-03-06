4644c757a71b6d0e0753c77bc44729c84cdbd2d0
Gracie <graciengainey@gmail.com>
2023-03-06 11:08:15 -0500

    tried to do a makeRow function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Your code here
+const drawingBoard = document.getElementById("theTable")
+const square = document.getElementsByTagName('td')[0]
+const row = document.createElement("tr")
+
+function makeRow(num) {
+    for (let i=0; i<num; i++){
+        drawingBoard.appendChild(row)
+        for (let i=0; i<20; i++){
+            row.appendChild(square)
+        }
+    }
+}
+makeRow(5)
